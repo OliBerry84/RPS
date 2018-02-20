0a93abdf307ebd81e1ca03932165d63fe2d4a768
Oli <oliver.berry84@gmail.com>
2018-02-20 15:22:37 +0000

    more feckin updates

--- models/rockpaperscissors.rb
+++ models/rockpaperscissors.rb
@@ -8,19 +8,19 @@ class RockPaperScissors
     random_number = rand(0..2)
     computer_chooses = @item2[random_number]
     if @item1 == computer_chooses
-      return "it's a draw"
+      return "It's a draw!"
     elsif @item1 == "rock" and computer_chooses == "paper"
       return "Computer wins with paper!"
     elsif @item1 == "rock" and computer_chooses == "scissors"
-      return "Player1 wins with rock!"
+      return "You win with rock!"
     elsif @item1 == "paper" and computer_chooses == "rock"
-      return "Player1 wins with paper!"
+      return "You win with paper!"
     elsif @item1 == "paper" and computer_chooses == "scissors"
       return "Computer wins with scissors!"
     elsif @item1 == "scissors" and computer_chooses == "rock"
       return "Computer wins with rock!"
     else
-      return  "Player1 wins with scissors!"
+      return  "You win with scissors!"
     end # if end
   end # def end
 end # class end
